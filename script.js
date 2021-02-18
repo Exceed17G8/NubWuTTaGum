75d469d8e61f7c511872e386e88d66838f056f62
keyboard2543 <keyboard2543@gmail.com>
2021-02-18 22:13:24 +0700

    Improve UI and implement graph into the page

--- script.js
+++ script.js
@@ -27,6 +27,44 @@ switch (date.getDay()) {
 document.getElementById("day").innerText = dayTH;
 document.getElementById("time").innerText = date.toLocaleString();
 
-setTimeout(function() {
-    window.location.reload(1);
-}, 1000);
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['1200', '1300', '1400', '1500', '1600', '1700'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+// setTimeout(function() {
+//     window.location.reload(1);
+// }, 1000);
