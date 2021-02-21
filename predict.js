3d39e2da32a1e3c3939f0c4626602660c3e87db1
keyboard2543 <keyboard2543@gmail.com>
2021-02-21 11:17:23 +0700

    NubWuTTaGum 1.5.2 pocket edition

--- predict.js
+++ predict.js
@@ -4,14 +4,14 @@ function update_prediction() {
         var today = date.getDay();
         var prediction_value = data["customerDensity"][(today+1)%7]; // Next day
         var prediction_customer = data["dayAvg"][(today+1)%7] // Next day
-        var prediction_content = `Tomorrow's average customer is ${Math.round(prediction_customer)} people. `;
+        var prediction_content = `Tomorrow's average customer is ${Math.round(prediction_customer)} people.\n`;
     
         if (prediction_value < 0.25) prediction_content += "So, U R chilled out tomorrow bro!";
-        else if (prediction_value >= 0.25 && prediction_value < 50) prediction_content += "Tomorrow is a normal day 4U";
-        else if (prediction_value >= 50 && prediction_value < 75) prediction_content += "U R busy in tomorrow";
-        else  prediction_content += "Prepare for your plenty of customers in tomorrow";
+        else if (prediction_value >= 0.25 && prediction_value < 50) prediction_content += "Tomorrow is a normal day for you.";
+        else if (prediction_value >= 50 && prediction_value < 75) prediction_content += "U R busy in tomorrow.";
+        else  prediction_content += "Prepare for your plenty of customers in tomorrow.";
     
-        document.getElementById("predict").setAttribute("data-bs-content", prediction_content);
+        document.getElementById("predict").innerText = prediction_content;
     });
 
     setTimeout(update_prediction, 1000);
